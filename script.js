8d977887a59aa065aa7419f0d274f81d94a194fa
RaphaelDeCarvalho <raphaeldecarvalho1@hotmail.com>
2022-01-27 19:02:00 -0300

    adicionado script para apresentar botao de rolagem para cima e para baixo

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+var buttonUp = document.querySelector('button.botaoUp');
+var buttonDown = document.querySelector('button.botaoDown');
+
 function scrollButton(){
     document.documentElement.scrollTop = 1601
+};
+function scrollBackButton(){
+    document.documentElement.scrollTop = 0
+}
+
+window.onscroll = function() {showScroll()};
+
+function showScroll() {
+  if (document.body.scrollTop > 22 || document.documentElement.scrollTop > 22) {
+    buttonDown.style.display = "none"; buttonUp.style.display = "block"
+  } else {
+    buttonDown.style.display = "block"; buttonUp.style.display = "none"
+  }
 }
