88131937600849a142befe57c16c3b89925ca3e8
RaphaelDeCarvalho <raphaeldecarvalho1@hotmail.com>
2022-11-01 17:55:22 -0300

    alterado função de script para rolagem da tela

--- script/script.js
+++ script/script.js
@@ -1,15 +1,37 @@
-var buttonUp = document.querySelector('button.botaoUp');
-var buttonDown = document.querySelector('button.botaoDown');
+let screenTop = document.querySelector('#toppage');
+let screenMiddle = document.querySelector('#middlepage');
+let screenBottom = document.querySelector('#bottompage');
 
-function scrollButton(){
-    document.documentElement.scrollTop = 1680
-};
+let buttonUp = document.querySelector('button.botaoUp');
+let buttonMiddle = document.querySelector('button.botaoMiddle');
+let buttonDown = document.querySelector('button.botaoDown');
+
+
+buttonUp.addEventListener('click', function(){
+  screenTop.scrollIntoView()
+});
+buttonMiddle.addEventListener('click', function(){
+  screenMiddle.scrollIntoView()
+});
+buttonDown.addEventListener('click', function(){
+  screenBottom.scrollIntoView()
+});
+
+/*
 function scrollBackButton(){
     document.documentElement.scrollTop = 0
-}
+};
+function scrollMiddleButton(){
+    document.documentElement.scrollTop = 840
+};
+function scrollBottomButton(){
+    document.documentElement.scrollTop = 1680
+};
+*/
 
-window.onscroll = function() {showScroll()};
+//window.onscroll = function() {showScroll()};
 
+/*
 function showScroll() {
   if (document.body.scrollTop > 22 || document.documentElement.scrollTop > 22) {
     buttonDown.style.display = "none"; buttonUp.style.display = "block"
@@ -17,3 +39,4 @@ function showScroll() {
     buttonDown.style.display = "block"; buttonUp.style.display = "none"
   }
 }
+*/
