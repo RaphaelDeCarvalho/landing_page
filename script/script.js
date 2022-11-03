0762a8e02e0f1185e8779fd4315f1250f8abf699
RaphaelDeCarvalho <raphaeldecarvalho1@hotmail.com>
2022-11-02 23:14:49 -0300

    alterado captura de elementos e adicionado eventos para navegação de tela

--- script/script.js
+++ script/script.js
@@ -5,18 +5,25 @@ let screenBottom = document.querySelector('#bottompage');
 let buttonUp = document.querySelector('button.botaoUp');
 let buttonMiddle = document.querySelector('button.botaoMiddle');
 let buttonDown = document.querySelector('button.botaoDown');
+let buttonTop = document.querySelector('button.botaoTop');
 
 
 buttonUp.addEventListener('click', function(){
   screenTop.scrollIntoView()
+  console.log('cliquei no up button')
 });
 buttonMiddle.addEventListener('click', function(){
   screenMiddle.scrollIntoView()
+  console.log('cliquei no middle button')
 });
 buttonDown.addEventListener('click', function(){
   screenBottom.scrollIntoView()
+  console.log('cliquei no down button')
 });
-
+buttonTop.addEventListener('click', function(){
+  screenTop.scrollIntoView()
+  console.log('cliquei no top button')
+});
 /*
 function scrollBackButton(){
     document.documentElement.scrollTop = 0
