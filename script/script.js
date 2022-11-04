386c40cb6aefcf3f08942a4efbd8ca70cad155a8
RaphaelDeCarvalho <raphaeldecarvalho1@hotmail.com>
2022-11-04 16:27:22 -0300

    alterado script para rolagem de tela e de exibição dinâmica das setas de navegação.

--- script/script.js
+++ script/script.js
@@ -7,43 +7,63 @@ let buttonMiddle = document.querySelector('button.botaoMiddle');
 let buttonDown = document.querySelector('button.botaoDown');
 let buttonTop = document.querySelector('button.botaoTop');
 
+let buttonLeft = document.querySelector('button.botaoLeft');
+let buttonRight = document.querySelector('button.botaoRight');
+
+buttonMiddle.addEventListener('click', function(){
+  screenMiddle.scrollIntoView()
+  console.log(`body.scrollTop = ${document.body.scrollTop} & 
+  documentElement.scrollTop = ${document.documentElement.scrollTop}`)
+});
 
 buttonUp.addEventListener('click', function(){
   screenTop.scrollIntoView()
-  console.log('cliquei no up button')
-});
-buttonMiddle.addEventListener('click', function(){
-  screenMiddle.scrollIntoView()
-  console.log('cliquei no middle button')
+  console.log(`body.scrollTop = ${document.body.scrollTop} & 
+  documentElement.scrollTop = ${document.documentElement.scrollTop}`)
 });
 buttonDown.addEventListener('click', function(){
   screenBottom.scrollIntoView()
-  console.log('cliquei no down button')
+  console.log(`body.scrollTop = ${document.body.scrollTop} & 
+  documentElement.scrollTop = ${document.documentElement.scrollTop}`)
 });
 buttonTop.addEventListener('click', function(){
   screenTop.scrollIntoView()
-  console.log('cliquei no top button')
+  console.log(`body.scrollTop = ${document.body.scrollTop} & 
+  documentElement.scrollTop = ${document.documentElement.scrollTop}`)
 });
-/*
-function scrollBackButton(){
-    document.documentElement.scrollTop = 0
-};
-function scrollMiddleButton(){
-    document.documentElement.scrollTop = 840
-};
-function scrollBottomButton(){
-    document.documentElement.scrollTop = 1680
-};
-*/
 
-//window.onscroll = function() {showScroll()};
+buttonLeft.addEventListener('click', function(){
+  screenTop.scrollIntoView()
+  console.log(`body.scroll( = ${document.body.scrollLeft} & 
+  documentElement.scroll( = ${document.documentElement.scrollLeft}`)
+});
+buttonRight.addEventListener('click', function(){
+  screenBottom.scrollIntoView()
+  console.log(`body.scroll( = ${document.body.scrollTop} & 
+  documentElement.scroll( = ${document.documentElement.scrollTop}`)
+});
 
-/*
+
+window.onscroll = function() {showScroll()};
 function showScroll() {
-  if (document.body.scrollTop > 22 || document.documentElement.scrollTop > 22) {
-    buttonDown.style.display = "none"; buttonUp.style.display = "block"
+  
+    if (document.body.scrollLeft > 0 || document.documentElement.scrollLeft > 48) {
+      buttonLeft.style.display = "block"; buttonRight.style.display = "none";
+    } else if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
+      buttonRight.style.display = "none";
+    } else {
+      buttonLeft.style.display = "none"; buttonRight.style.display = "block";
+    } 
+  
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 100) {
+    buttonMiddle.style.display = "none"; buttonUp.style.display = "block";
   } else {
-    buttonDown.style.display = "block"; buttonUp.style.display = "none"
+    buttonMiddle.style.display = "block"; buttonUp.style.display = "none";
+  } 
+  
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 1600) {
+    buttonTop.style.display = "block"; buttonDown.style.display = "none";
+  } else {
+    buttonTop.style.display = "none"; buttonDown.style.display = "block";
   }
 }
-*/
